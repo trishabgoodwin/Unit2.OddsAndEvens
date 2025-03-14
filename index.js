1fda1b0e252a0c2dc4fd2305f29d2d9fe7869f39
Trisha Goodwin <trishabromero@gmail.com>
2025-03-13 20:24:13 -0500

    getting started

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // TODO: this file! :)
+const state = {
+    numbers: [],
+  };
+
+//When the user clicks the "Add Number" button, the number they entered into the input field should be added to the number bank.
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const numberInput = document.querySelector(number);
+  for (let i = 0; i < numberInput.value; i++) {
+    state.start.push(number);
+  }
+
+  renderNumbers();
+});
